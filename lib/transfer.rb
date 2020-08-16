6a56985c4d00d8c1108f58b7140dca462657c486
Nick Bazaldua <bazalnick@gmail.com>
2020-08-16 23:16:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,14 @@
 class Transfer
+  attr_accessor :sender, :reciever, :amount
 
 
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    
+  end
 
 
 
